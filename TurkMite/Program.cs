6e14fdba402c6583ac8d5a15326e1ca17fc353d2
Kristof <kristof@aut.bme.hu>
2018-06-13 13:17:58 +0200

    Also return delta direction, more compact and functional.

--- TurkMite/Program.cs
+++ TurkMite/Program.cs
@@ -30,18 +30,9 @@ namespace TurkMite
             {
             }
 
-            protected override Vec3b GetNextColorAndUpdateDirection(Vec3b currentColor)
+            protected override (Vec3b newColor, int deltaDirection) GetNextColorAndUpdateDirection(Vec3b currentColor)
             {
-                if (currentColor == black)
-                {
-                    direction++;
-                    return white;
-                }
-                else
-                {
-                    direction--;
-                    return black;
-                }
+                return (currentColor == black) ? (white, 1) : (black, -1);
             }
         }
 
@@ -51,7 +42,7 @@ namespace TurkMite
             private Mat.Indexer<Vec3b> indexer;
             private int x;
             private int y;
-            protected int direction;  // 0 up, 1 right, 2 down, 3 left
+            private int direction;  // 0 up, 1 right, 2 down, 3 left
             public TurkmiteBase(Mat image)
             {
                 Image = image;
@@ -65,12 +56,15 @@ namespace TurkMite
 
             public void Step()
             {
-                indexer[y, x] = GetNextColorAndUpdateDirection(indexer[y, x]);
-                PerformMove();
+                int deltaDirection;
+                (indexer[y, x], deltaDirection) =
+                    GetNextColorAndUpdateDirection(indexer[y, x]);
+                PerformMove(deltaDirection);
             }
 
-            private void PerformMove()
+            private void PerformMove(int deltaDirection)
             {
+                direction += deltaDirection;
                 direction = (direction + 4) % 4;
                 x += delta[direction].x;
                 y += delta[direction].y;
@@ -78,7 +72,7 @@ namespace TurkMite
                 y = Math.Max(0, Math.Min(Image.Rows, y));
             }
 
-            protected abstract Vec3b GetNextColorAndUpdateDirection(Vec3b currentColor);
+            protected abstract (Vec3b newColor, int deltaDirection) GetNextColorAndUpdateDirection(Vec3b currentColor);
         }
     }
 }
