9691a2c5902b269cc2a28398889ee9ec1870a688
Kristof <kristof@aut.bme.hu>
2018-06-13 13:11:30 +0200

    Extracted TurkmiteBase

--- TurkMite/Program.cs
+++ TurkMite/Program.cs
@@ -12,7 +12,7 @@ namespace TurkMite
         static void Main(string[] args)
         {
             Mat img = new Mat(200, 200, MatType.CV_8UC3, new Scalar(0, 0, 0));
-            var turkmite = new TurkMite(img);
+            var turkmite = new OriginalTurkmite(img);
             for(int i=0; i<13000; i++)
             {
                 turkmite.Step();
@@ -21,14 +21,38 @@ namespace TurkMite
             Cv2.WaitKey();
         }
 
-        class TurkMite
+        class OriginalTurkmite : TurkmiteBase
+        {
+            readonly private Vec3b black = new Vec3b(0, 0, 0);
+            readonly private Vec3b white = new Vec3b(255, 255, 255);
+
+            public OriginalTurkmite(Mat image) : base(image)
+            {
+            }
+
+            protected override Vec3b GetNextColorAndUpdateDirection(Vec3b currentColor)
+            {
+                if (currentColor == black)
+                {
+                    direction++;
+                    return white;
+                }
+                else
+                {
+                    direction--;
+                    return black;
+                }
+            }
+        }
+
+        abstract class TurkmiteBase
         {
             public Mat Image { get; }
             private Mat.Indexer<Vec3b> indexer;
             private int x;
             private int y;
-            private int direction;  // 0 up, 1 right, 2 down, 3 left
-            public TurkMite(Mat image)
+            protected int direction;  // 0 up, 1 right, 2 down, 3 left
+            public TurkmiteBase(Mat image)
             {
                 Image = image;
                 x = image.Cols / 2;
@@ -37,8 +61,6 @@ namespace TurkMite
                 indexer = image.GetGenericIndexer<Vec3b>();
             }
 
-            readonly private Vec3b black = new Vec3b(0, 0, 0);
-            readonly private Vec3b white = new Vec3b(255, 255, 255);
             readonly private (int x,int y)[] delta = new(int x, int y)[] { (0, -1), (1, 0), (0, 1), (-1, 0) };
 
             public void Step()
@@ -56,20 +78,7 @@ namespace TurkMite
                 y = Math.Max(0, Math.Min(Image.Rows, y));
             }
 
-            private Vec3b GetNextColorAndUpdateDirection(Vec3b currentColor)
-            {
-                if (currentColor == black)
-                {
-                    direction++;
-                    return white;
-                }
-                else
-                {
-                    direction--;
-                    return black;
-                }
-            }
-
+            protected abstract Vec3b GetNextColorAndUpdateDirection(Vec3b currentColor);
         }
     }
 }
